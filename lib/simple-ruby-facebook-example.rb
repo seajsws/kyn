00519e7308437d4911dd10d4429eba642c2100b6
BeeDesk -- Your desk away from your desk <tyip@beedesk.com>
2011-07-20 20:03:06 -0700

    Updated fixes from another project.

--- lib/simple-ruby-facebook-example.rb
+++ lib/simple-ruby-facebook-example.rb
@@ -23,7 +23,7 @@ class SimpleRubyFacebookExample < Sinatra::Application
 
 	get '/test' do
 		if session['access_token']
-		  'You are logged in! <a href="/logout">Logout</a>. Token '
+		  'You are logged in! <a href="/fblogout">Logout</a>. Token '
 		  session['access_token']
 			# do some stuff with facebook here
 			# for example:
@@ -33,13 +33,13 @@ class SimpleRubyFacebookExample < Sinatra::Application
 			# or publish to someone else (if you have the permissions too ;) )
 			# @graph.put_wall_post("Checkout my new cool app!", {}, "someoneelse's id")			
 		else
-			'<a href="/login">Login</a>'
+			'<a href="/fblogin">Login</a>'
 		end
 	end
 
 	post '/test' do
 		if session['access_token']
-		  'You are logged in! <a href="/logout">Logout</a>'
+		  'You are logged in! <a href="/fblogout">Logout</a>'
 			# do some stuff with facebook here
 			# for example:
 			# @graph = Koala::Facebook::GraphAPI.new(session["access_token"])
@@ -48,7 +48,7 @@ class SimpleRubyFacebookExample < Sinatra::Application
 			# or publish to someone else (if you have the permissions too ;) )
 			# @graph.put_wall_post("Checkout my new cool app!", {}, "someoneelse's id")			
 		else
-			'<a href="/login">Login</a>'
+			'<a href="/fblogin">Login</a>'
 		end
     end
 
@@ -56,14 +56,14 @@ class SimpleRubyFacebookExample < Sinatra::Application
     redirect '/index.html'
   end
 
-	get '/login' do
+	get '/fblogin' do
 		# generate a new oauth object with your app data and your callback url
 		session['oauth'] = Facebook::OAuth.new(APP_ID, APP_CODE, SITE_URL + 'callback')
 		# redirect to facebook to get your code
 		redirect session['oauth'].url_for_oauth_code()
 	end
 
-	get '/logout' do
+	get '/fblogout' do
 		session['oauth'] = nil
 		session['access_token'] = nil
 		redirect '/'
@@ -93,12 +93,17 @@ class SimpleRubyFacebookExample < Sinatra::Application
     "put to #{DB}/#{doc}?#{args}"
     request.body.rewind
     data = request.body.read
-    doc = RestClient.put("#{DB}/#{doc}?_rev=#{args}", data) { |dbresponse, dbrequest, dbresult|
+    if (args.nil?)
+      path = "#{DB}/#{doc}"
+    else 
+      path = "#{DB}/#{doc}?_rev=#{args}"
+    end
+    doc = RestClient.put(path, data) { |dbresponse, dbrequest, dbresult|
       case dbresponse.code
       when 200, 201, 202
         dbresponse
       else
-        halt dbresponse.code, "put to #{DB}/#{doc}?rev=#{args} [#{data}] - pass thru '%s'<br> '%s'!" % [dbresponse.code, dbresponse.body] 
+        halt dbresponse.code, "post to #{path} [#{data}] - pass thru '#{dbresponse.code}'<br> '#{dbresponse.body}'!" 
       end
     }
   end
@@ -107,13 +112,18 @@ class SimpleRubyFacebookExample < Sinatra::Application
     "post to #{DB}/#{doc}?#{args}"
     request.body.rewind
     data = request.body.read
-    doc = RestClient.put("#{DB}/#{doc}?_rev=#{args}", data) { |dbresponse, dbrequest, dbresult|
+    if (args.nil?)
+      path = "#{DB}/#{doc}"
+    else
+      path = "#{DB}/#{doc}?_rev=#{args}"
+    end
+    doc = RestClient.put(path, data) { |dbresponse, dbrequest, dbresult|
       case dbresponse.code
       when 200, 201, 202
         dbresponse
       else
         request.body.rewind
-        halt dbresponse.code, "post to #{DB}/#{doc}?rev=#{args} [#{data}] - pass thru '%s'<br> '%s'!" % [dbresponse.code, dbresponse.body] 
+        halt dbresponse.code, "post to #{path} [#{data}] - pass thru '#{dbresponse.code}'<br> '#{dbresponse.body}'!" 
       end
     }
   end
