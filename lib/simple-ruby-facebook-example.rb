6dc873f777355df7ad3a6520856f830eddaf2408
BeeDesk -- Your desk away from your desk <tyip@beedesk.com>
2011-07-27 20:23:43 -0700

    Removed koala.

--- lib/simple-ruby-facebook-example.rb
+++ lib/simple-ruby-facebook-example.rb
@@ -2,16 +2,11 @@ APP_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
 require 'rubygems'
 require 'sinatra'
-require 'koala'
 require 'net/http'
 require 'rest-client'
 require 'json'
 require 'haml'
 
-# register your app at facebook to get those infos
-APP_ID = 191240154256553 # your app id
-APP_CODE = '0a211f68c547127f02e766b1c9f9c43b' # your app code
-SITE_URL = 'http://fb.gravify.com/' # your app site url
 DB = "#{ENV['CLOUDANT_URL']}/jdata"
 
 class SimpleRubyFacebookExample < Sinatra::Application
@@ -21,61 +16,10 @@ class SimpleRubyFacebookExample < Sinatra::Application
 	set :root, APP_ROOT
 	enable :sessions
 
-	get '/test' do
-		if session['access_token']
-		  'You are logged in! <a href="/fblogout">Logout</a>. Token '
-		  session['access_token']
-			# do some stuff with facebook here
-			# for example:
-			# @graph = Koala::Facebook::GraphAPI.new(session["access_token"])
-			# publish to your wall (if you have the permissions)
-			# @graph.put_wall_post("I'm posting from my new cool app!")
-			# or publish to someone else (if you have the permissions too ;) )
-			# @graph.put_wall_post("Checkout my new cool app!", {}, "someoneelse's id")			
-		else
-			'<a href="/fblogin">Login</a>'
-		end
-	end
-
-	post '/test' do
-		if session['access_token']
-		  'You are logged in! <a href="/fblogout">Logout</a>'
-			# do some stuff with facebook here
-			# for example:
-			# @graph = Koala::Facebook::GraphAPI.new(session["access_token"])
-			# publish to your wall (if you have the permissions)
-			# @graph.put_wall_post("I'm posting from my new cool app!")
-			# or publish to someone else (if you have the permissions too ;) )
-			# @graph.put_wall_post("Checkout my new cool app!", {}, "someoneelse's id")			
-		else
-			'<a href="/fblogin">Login</a>'
-		end
-    end
 
   get '/' do
     redirect '/index.html'
   end
-
-	get '/fblogin' do
-		# generate a new oauth object with your app data and your callback url
-		session['oauth'] = Facebook::OAuth.new(APP_ID, APP_CODE, SITE_URL + 'callback')
-		# redirect to facebook to get your code
-		redirect session['oauth'].url_for_oauth_code()
-	end
-
-	get '/fblogout' do
-		session['oauth'] = nil
-		session['access_token'] = nil
-		redirect '/'
-	end
-
-	#method to handle the redirect from facebook back to you
-	get '/callback' do
-		#get the access token from facebook with your code
-
-		session['access_token'] = session['oauth'].get_access_token(params[:code])
-		redirect '/'
-	end
 
   get '/jdata/:doc' do
     RestClient.get("#{DB}/#{params[:doc]}") { |response, request, result|
